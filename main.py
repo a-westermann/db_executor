22eab2b8b5a6834684a3318b689bf8ffddb5a358
Vierce1 <a.westermann.19@gmail.com>
2024-10-04 15:26:04 -0500

    pathing

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 import psycopg2
 import json
+import sys, os
+sys.path.insert(0, os.path.abspath('.'))
 
 
 def execute(command: str):
     print(f'executing: {command}')
-    db_info = json.load(open('root/home/ubuntu/db_executor/db.json'))
+    db_info = json.load(open('db.json'))
     conn = psycopg2.connect(database=db_info['database'], user=db_info['user'],
             password=db_info['password'], host=db_info['host'], port=db_info['port'])
     cursor = conn.cursor()
@@ -17,7 +19,7 @@ def execute(command: str):
 
 if __name__ == '__main__':
     # read the file, see if any commands need to execute
-    command_file = open('root/home/ubuntu/db_executor/commands.txt').read()
+    command_file = open('commands.txt').read()
     for line in command_file.split('\n')[1:]:
         last_run_time, interval_s, sql = line.split('\t')
         elapsed = datetime.now() - datetime.fromisoformat(last_run_time)
