b4df2f60ba0c6d79e314a0123768c5718007146c
Vierce1 <a.westermann.19@gmail.com>
2024-10-04 16:01:53 -0500

    pathing

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # read the file, see if any commands need to execute
     command_file = open('commands.txt').read()
     for line in command_file.split('\n')[1:]:
-        last_run_time, interval_s, sql = line.split('\t')
+        last_run_time, interval_s, sql = line.split('>')
         elapsed = datetime.now() - datetime.fromisoformat(last_run_time)
         if elapsed.total_seconds() > float(interval_s):
             execute(sql)
